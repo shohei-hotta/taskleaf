ec43bb51a1a2d0f25a39b56cd384fc9112f22f1d
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-19 09:28:55 +0000

    Chapter4-8まで

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
+  scope :recent, -> { order(created_at: :desc) }
+
   private
 
   def validate_name_not_including_comma
